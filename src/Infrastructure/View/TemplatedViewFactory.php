6dd0c2df68cea9a34abb343a67742349db996d7d
Viktor Szépe <viktor@szepe.net>
2020-08-18 06:13:11 +0200

    Do not rely on core in TemplatedViewFactory

--- src/Infrastructure/View/TemplatedViewFactory.php
+++ src/Infrastructure/View/TemplatedViewFactory.php
@@ -71,11 +71,11 @@ final class TemplatedViewFactory implements Service, ViewFactory {
 			$default_locations = ( function_exists( 'get_stylesheet_directory' )
 			                       && function_exists( 'get_template_directory' ) )
 				? [
-					trailingslashit( \get_stylesheet_directory() ) . 'views/',
-					trailingslashit( \get_template_directory() ) . 'views/',
-					trailingslashit( \dirname( __DIR__, 3 ) ) . 'views/',
+					\trailingslashit( \get_stylesheet_directory() ) . 'views/',
+					\trailingslashit( \get_template_directory() ) . 'views/',
+					\trailingslashit( \dirname( __DIR__, 3 ) ) . 'views/',
 				]
-				: [ trailingslashit( \dirname( __DIR__, 3 ) ) . 'views/' ];
+				: [ \dirname( __DIR__, 3 ) . 'views/' ];
 		}
 
 		return $default_locations;
